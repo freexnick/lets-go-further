44d43503f140b05046a05eb86e9a0bdab430daed
Nika <freexnick@yahoo.com>
2024-03-17 23:59:54 +0400

    chapter5:db_psql

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
-	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
 
 type config struct {
-	port uint
+	port string
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -22,13 +29,27 @@ type application struct {
 }
 
 func main() {
-	var cfg config
+	cfg, err := loadEnvConfig()
+	if err != nil {
+		panic(err)
+	}
 
-	flag.UintVar(&cfg.port, "port", 3000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.port, "port", cfg.port, "API server port")
+	flag.StringVar(&cfg.env, "env", cfg.env, "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", cfg.db.dsn, "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	app := application{
 		config: cfg,
@@ -36,7 +57,7 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Addr:         cfg.port,
 		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
@@ -44,7 +65,39 @@ func main() {
 		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func loadEnvConfig() (config, error) {
+	var cfg config
+	err := godotenv.Load()
+	if err != nil {
+		return cfg, err
+	}
+
+	cfg.port = os.Getenv("PORT")
+	cfg.env = os.Getenv("ENV")
+	cfg.db.dsn = os.Getenv("PSQL_DSN")
+
+	return cfg, nil
 }
