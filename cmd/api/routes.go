cc8473455b46837850a54ec51418a0d1a5e184d6
Nika <freexnick@yahoo.com>
2024-03-18 23:30:26 +0400

    chapter9:query

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,6 +6,7 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("/v1/movies", app.listMoviesHandler)
 	mux.HandleFunc("/v1/movies/{id}", app.showMovieHandler)
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
 	mux.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
