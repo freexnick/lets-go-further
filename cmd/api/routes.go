82eaf99b95e1434d9935542df60a3f88e0935527
Nika <freexnick@yahoo.com>
2024-04-10 22:15:04 +0400

    chapter16:auth_checks

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,11 +6,12 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-	mux.HandleFunc("/v1/movies", app.listMoviesHandler)
-	mux.HandleFunc("/v1/movies/{id}", app.showMovieHandler)
-	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
-	mux.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
-	mux.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
+
+	mux.HandleFunc("/v1/movies", app.requireActivatedUser(app.listMoviesHandler))
+	mux.HandleFunc("/v1/movies/{id}", app.requireActivatedUser(app.showMovieHandler))
+	mux.HandleFunc("POST /v1/movies", app.requireActivatedUser(app.createMovieHandler))
+	mux.HandleFunc("PATCH /v1/movies/{id}", app.requireActivatedUser(app.updateMovieHandler))
+	mux.HandleFunc("DELETE /v1/movies/{id}", app.requireActivatedUser(app.deleteMovieHandler))
 
 	mux.HandleFunc("POST /v1/users", app.registerUserHandler)
 	mux.HandleFunc("PUT /v1/users/activated", app.activateUserHandler)
